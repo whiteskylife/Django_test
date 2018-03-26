e107811f686fe7f316388a34d48b456dd09b596d
Administrator <981577040@qq.com>
2018-03-26 09:48:06 +0800

    django--login

--- cmdb/views.py
+++ cmdb/views.py
@@ -3,10 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def login(request):
-    return render(request, 'login.html')
+    """
+    :param request:包含了用户端请求提交的所有的信息
+    :return:
+    """
+    # print(request.method)
+    error_msg = ''
+    user = request.POST.get('user', None)
+    pwd = request.POST.get('pwd', None)
+    if user == 'whisky' and pwd == '123':
+        return redirect('http://www.baidu.com')
+    else:
+        error_msg = '用户名密码错误！'
+    return render(request, 'login.html', {'error_msg': error_msg})
 
 
 def home(request):
